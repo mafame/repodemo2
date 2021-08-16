038456aa670cb066f333616611f6bb85f06c0f4b
amelieolga <mafotsingf@gmail.com>
2021-08-16 13:16:18 +0000

    my parctice commit

--- DBConnect.java
+++ DBConnect.java
@@ -2,3 +2,5 @@ this is git. We are learning git and how to commit codes on git using github.
 this is really interesting.
 I am going to give it my all to succeed and obtain a good paying job that will allow me to invest and secure my future and that of my kids.
 Thank you Landmark
+I am learning everyday
+
